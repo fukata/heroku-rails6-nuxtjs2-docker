9b605525e434e88480bd77faa65c9b290a1a77a3
fukata <tatsuya.fukata@gmail.com>
2021-09-12 14:55:21 +0900

    読み込み中の表示を追加

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -5,24 +5,29 @@
 
       <button @click="loadDummyData">データを読み込む</button>
 
-      <template v-if="dummyData.length > 0">
-        <table>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>NAME</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="r in dummyData" :key="r.id">
-              <td>{{ r.id }}</td>
-              <td>{{ r.name }}</td>
-            </tr>
-          </tbody>
-        </table>
+      <template v-if="loading">
+        <p>データを読み込んでいます。しばらくお待ちください。</p>
       </template>
       <template v-else>
-        <p>データを読み込んでください。</p>
+        <template v-if="dummyData.length > 0">
+          <table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>NAME</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="r in dummyData" :key="r.id">
+                <td>{{ r.id }}</td>
+                <td>{{ r.name }}</td>
+              </tr>
+            </tbody>
+          </table>
+        </template>
+        <template v-else>
+          <p>データを読み込んでください。</p>
+        </template>
       </template>
     </div>
   </div>
@@ -33,13 +38,22 @@ export default {
   data() {
     return {
       dummyData: {},
+      loading: false,
     }
   },
   methods: {
     async loadDummyData() {
-      const resp = await this.$axios.$get('/dummy')
-      console.log(resp)
-      this.dummyData = resp.data
+      try {
+        this.loading = true
+
+        const resp = await this.$axios.$get('/dummy')
+        console.log(resp)
+        this.dummyData = resp.data
+      } catch (e) {
+        console.error(e)
+      } finally {
+        this.loading = false
+      }
     },
   },
 }
